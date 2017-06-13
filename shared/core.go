83660d2351fb850f8cbc1a6e6b311e46b1191e74
pcrawfor <pcrawfor@gmail.com>
2017-06-13 12:53:19 -0600

    Fixing uuid formatting

--- shared/core.go
+++ shared/core.go
@@ -2,14 +2,24 @@ package shared
 
 import (
 	"strconv"
+	"strings"
 
 	"code.google.com/p/go-uuid/uuid"
 )
 
+// ChannelHandshake handshake channel path
 const ChannelHandshake = "/meta/handshake"
+
+// ChannelConnect connect channel path
 const ChannelConnect = "/meta/connect"
+
+// ChannelDisconnect disconnect channel path
 const ChannelDisconnect = "/meta/disconnect"
+
+// ChannelSubscribe subscribe channel path
 const ChannelSubscribe = "/meta/subscribe"
+
+// ChannelUnsubscribe unsubscribe channel path
 const ChannelUnsubscribe = "/meta/unsubscribe"
 
 // Core shared functions
@@ -25,5 +35,6 @@ func (c *Core) NextMessageID() string {
 
 // GenerateClientID generates a new UUID string
 func (c *Core) GenerateClientID() string {
-	return uuid.New()
+	rawID := uuid.New()
+	return strings.Replace(rawID, "-", "", -1)
 }
